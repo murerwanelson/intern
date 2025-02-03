09ad92587721cb110df2e4144aff9db4ebd0babe
murerwanelson <145276589+murerwanelson@users.noreply.github.com>
2025-02-03 10:21:16 +0200

    .

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Documents\GitHub\intern\env
+set VIRTUAL_ENV=C:\Users\user\Desktop\nellah\env
 
 if not defined PROMPT set PROMPT=$P$G
 
--- zingsa_project/zingsa_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('zingsa/', include('zingsa_app.urls')),
-    path('', include('zingsa_app.urls')), 
-]
